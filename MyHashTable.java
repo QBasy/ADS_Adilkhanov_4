253ce679d6dcae5d1498582b84c7fd99eb3153db
QoidynBasy <c0mplexie1@gmail.com>
2023-05-04 14:36:22 +0600

    Second commit

---
+++ MyHashTable.java
@@ -0,0 +1,30 @@
+public class MyHashTable<K, V> extends HashNode
+{
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+
+    public MyHashTable()
+    {}
+
+    public MyHashTable(int M)
+    {}
+
+    private int hash(K key)
+    {}
+
+    public void put(K key, V value)
+    {}
+
+    public V get(K key)
+    {}
+
+    public V remove(K key)
+    {}
+
+    public boolean contains(V value)
+    {}
+
+    public K getKey(V value)
+    {}
+}
