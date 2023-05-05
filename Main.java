18d908eb4ea6f298a8dc76bd2ae285d89704779a
QoidynBasy <c0mplexie1@gmail.com>
2023-05-05 09:25:32 +0600

    fifth commit

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
     }
-=======
-package PACKAGE_NAME;public class Main {
->>>>>>> 39816c9537b7262366a6b2ee115809250d9af614
 }
