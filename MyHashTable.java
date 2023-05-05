df1b612f3a9adeed418c0f3b5b915a475a9e21ad
QoidynBasy <c0mplexie1@gmail.com>
2023-05-05 08:03:53 +0600

    fourth commit

--- MyHashTable.java
+++ MyHashTable.java
@@ -6,25 +6,18 @@ public class MyHashTable<K, V> extends HashNode
 
     public MyHashTable()
     {}
-
     public MyHashTable(int M)
     {}
-
     private int hash(K key)
     {}
-
     public void put(K key, V value)
     {}
-
     public V get(K key)
     {}
-
     public V remove(K key)
     {}
-
     public boolean contains(V value)
     {}
-
     public K getKey(V value)
     {}
 }
