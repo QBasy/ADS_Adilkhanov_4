9f515da6293787aca92ff436fdd560cf4335283a
QoidynBasy <c0mplexie1@gmail.com>
2023-05-12 18:29:49 +0600

    new com

--- Main.java
+++ Main.java
@@ -1,12 +1,1 @@
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        MyHashTable<Integer, String> ht = new MyHashTable<>();
-        Scanner scanner = new Scanner(System.in);
-        String n = scanner.next();
-        int a = scanner.nextInt();
-        ht.put(a , n);
-        System.out.println(ht.getKey(n));
-    }
-}
